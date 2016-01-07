6efb48e5d7a47d2246da717e02af8795c98d7557
NicolasMugnier <mugnier.nicolas@gmail.com>
2016-01-07 16:45:06 +0100

    init sample hello world module

---
+++ app/code/Samples/Helloworld/Block/Index.php
@@ -0,0 +1,11 @@
+<?php namespace Samples\Helloworld\Block;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+    /*public function __construct(){
+
+        //die('88');
+
+    }*/
+
+}
